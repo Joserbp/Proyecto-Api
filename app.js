b658df4519df9185fdef227b33a161213f2914d9
Joserbp <josera.jack@gmail.com>
2019-05-24 20:09:40 -0500

    Se agrega metodos GET y DELETE completos y conexion a mongoose en app.js

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//conexión a Mongoose
+var mongoose = require('mongoose');
+var Alumno = require('./models/Automoviles.js');
+var user='joserbp';
+var password='S2KzDvgJShI9h8Ke';
+
+mongoose.connect('mongodb+srv://'+user+':'+password+'@cluster0-p1uhr.gcp.mongodb.net/IngSoftware?retryWrites=true', {
+  useNewUrlParser: true
+}).then(() => { console.log('Conectado a Mongo DB Atlas')})
+.catch(err => console.log(err));
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var automovilesRouter=require('./routes/Autos.js');
 
 var app = express();
 
@@ -22,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/auto', automovilesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/Autos.js
+++ routes/Autos.js
@@ -4,14 +4,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Automovil= require('../models/Automoviles');
 
-router.get('/', function(req, res, next) {
-  Computadora.find({},function(err,datos){
-    res.status(200).json(datos);
-  });
-
-});
-
-/* nuevo Automovil*/
+//POST de un auto nuevo
 router.post('/', function(req, res, next) {
   console.log(req.body);
   var auto=  Automovil({
@@ -27,10 +20,52 @@ router.post('/', function(req, res, next) {
     if (err) {
       res.send('error');
     }else {
-      res.send(data);
+      res.status(200).send(data);
     }
   });
 
+});
+
+//GET de todos los elementos
+router.get('/', function(req, res, next) {
+  Computadora.find({},function(err,datos){
+    res.status(200).json(datos);
+  });
+
+});
+
+//GET por un ID
+router.get('/:AutoId', function(req, res, next) {
+  Automovil.findOne({
+    'id': req.params.AutoId
+  }, function(err, datos) {
+    if (datos == null) {
+      res.status(404).json({
+        mensaje: "No existe"
+      });
+    } else {
+      res.status(200).json(datos);
+    }
+
+  });
+  //res.json(req.params.userId);
+});
+
+//DELETE para un ID
+router.delete('/:AutoId', function(req, res, next) {
+  Automovil.findOneAndDelete({
+    id: req.params.AutoId
+  }, function(err, data) {
+    if (err) {
+      res.status(404).json(err);
+    }
+    res.status(200).json(data);
+  });
+});
+
+//DELETE para toda la base
+router.delete('/',function(req,res,next){
+  res.status(405).json({mensaje:'Accion no permitida'});
 });
 
 module.exports = router;
